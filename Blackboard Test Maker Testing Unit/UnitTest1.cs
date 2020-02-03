73ff9339cd4e76bcf58255cea78cef87ed9a7f7a
ino-bar <ino-bar@users.noreply.github.com>
2020-02-03 10:56:15 +0000

    03-02-20 added project
    Added new project for making Blackboard tests from Word files converted to HTML.

---
+++ Blackboard Test Maker Testing Unit/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Blackboard_Test_Maker__from_HTML_;
+
+namespace Blackboard_Test_Maker_Testing_Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(8.0, 5 + 3, 0);
+        }
+    }
+}
